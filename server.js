436ec222292dee58b1ecfdda205e931ab96cf4d1
suvonov2310-a11y <suvonov2310@gmail.com>
2026-01-13 15:37:14 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -8,20 +8,21 @@ app.use(express.static(__dirname));
 io.on('connection', (socket) => {
     socket.on('join-room', (roomId) => {
         socket.join(roomId);
-        const numClients = io.sockets.adapter.rooms.get(roomId)?.size || 0;
+        const clients = io.sockets.adapter.rooms.get(roomId);
+        const numClients = clients ? clients.size : 0;
         
-        // Ikkala o'yinchiga darhol boshlash xabarini yuborish
+        // O'yinchilarga nechanchi bo'lib kirganini yuborish
         io.to(roomId).emit('status-update', {
             msg: numClients >= 2 ? "O'yin boshlandi!" : "Raqib kutilmoqda...",
-            active: numClients >= 2
+            active: numClients >= 2,
+            count: numClients
         });
     });
 
     socket.on('move', (data) => {
-        // Yurishni faqat raqibga yuboramiz
         socket.to(data.roomId).emit('move-received', data.move);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log('Server running'));
+http.listen(PORT, () => console.log('Server is running...'));
