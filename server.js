2aa9f683037f580b640c24605daeb101c4b7b908
suvonov2310-a11y <suvonov2310@gmail.com>
2026-01-15 17:59:54 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -5,18 +5,66 @@ const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
-let activePlayers = {};
+let onlineUsers = {}; 
 
 io.on('connection', (socket) => {
+    console.log('Foydalanuvchi ulandi:', socket.id);
+
     socket.on('join-lobby', (user) => {
-        activePlayers[socket.id] = { ...user, id: socket.id };
-        io.emit('update-users', Object.values(activePlayers));
+        // Foydalanuvchi ma'lumotlariga socket.id ni qat'iy biriktiramiz
+        onlineUsers[socket.id] = { 
+            id: user.id, 
+            name: user.name, 
+            socketId: socket.id 
+        };
+        io.emit('update-users', Object.values(onlineUsers));
+    });
+
+    socket.on('send-invite', (data) => {
+        // Taklif yuborilayotgan odamni qidirish
+        const targetUser = Object.values(onlineUsers).find(u => u.id === data.toId);
+        if (targetUser) {
+            console.log(`Taklif ketdi: ${data.fromName} -> ${targetUser.name}`);
+            io.to(targetUser.socketId).emit('receive-invite', data);
+        }
+    });
+
+    socket.on('accept-invite', (data) => {
+        const roomId = "room_" + Math.random().toString(36).slice(2, 9);
+        
+        // Har ikkala o'yinchining hozirgi socket ob'ektlarini topamiz
+        const sender = Object.values(onlineUsers).find(u => u.id === data.fromId);
+        const receiver = Object.values(onlineUsers).find(u => u.id === data.toId);
+
+        if (sender && receiver) {
+            const sSocket = io.sockets.sockets.get(sender.socketId);
+            const rSocket = io.sockets.sockets.get(receiver.socketId);
+
+            if (sSocket && rSocket) {
+                // Ikkalasini ham xonaga kirgizamiz
+                sSocket.join(roomId);
+                rSocket.join(roomId);
+
+                const roles = {};
+                roles[data.fromId] = 'red';
+                roles[data.toId] = 'light';
+
+                console.log(`O'yin boshlandi: ${roomId}`);
+                io.to(roomId).emit('start-game', { roomId, players: roles });
+            }
+        }
+    });
+
+    socket.on('make-move', (data) => {
+        // Xonadagi boshqa o'yinchiga yurishni yuborish
+        socket.to(data.roomId).emit('move-made', data);
     });
 
     socket.on('disconnect', () => {
-        delete activePlayers[socket.id];
-        io.emit('update-users', Object.values(activePlayers));
+        delete onlineUsers[socket.id];
+        io.emit('update-users', Object.values(onlineUsers));
     });
 });
 
-http.listen(process.env.PORT || 3000, () => console.log('Server ishga tushdi'));
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => console.log(`Server ${PORT}-portda yondi`));
