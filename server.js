df94254d06c68c3e103a8457d0893eebc663e359
suvonov2310-a11y <suvonov2310@gmail.com>
2026-01-21 15:36:23 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -7,64 +7,36 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '/')));
 
-// Oddiy ma'lumotlar ombori (Vaqtinchalik)
-let users = {}; 
-let rooms = {};
+let users = {};
 
 io.on('connection', (socket) => {
-    console.log('Ulandi:', socket.id);
-
-    // Lobbyga qo'shilish
+    // Foydalanuvchi lobbiga kirganda
     socket.on('join-lobby', (data) => {
-        users[socket.id] = {
-            id: socket.id,
-            name: data.name,
-            xp: 500, // Standart XP
-            wins: 0,
-            losses: 0
-        };
-        
-        // Profil ma'lumotlarini qaytarish
-        socket.emit('profile-data', users[socket.id]);
-        
-        // Hammaning ro'yxatini yangilash
-        io.emit('update-users', Object.values(users));
+        users[socket.id] = { id: socket.id, name: data.name, xp: 500 };
+        io.emit('update-users', Object.values(users)); // Hammaga yangi ro'yxatni yuborish
     });
 
     // Taklif yuborish
     socket.on('send-invite', (data) => {
-        const fromUser = users[socket.id];
         if (users[data.toId]) {
-            io.to(data.toId).emit('receive-invite', {
-                fromId: socket.id,
-                fromName: fromUser.name
+            io.to(data.toId).emit('receive-invite', { 
+                fromId: socket.id, 
+                fromName: data.fromName 
             });
         }
     });
 
-    // Taklifni qabul qilish va o'yinni boshlash
+    // Taklifni qabul qilish
     socket.on('accept-invite', (data) => {
-        const roomId = `room_${data.fromId}_${socket.id}`;
+        const roomId = `room-${socket.id}-${data.fromId}`;
         const players = {};
-        players[data.fromId] = 'red';
-        players[socket.id] = 'white';
+        players[socket.id] = 'red';
+        players[data.fromId] = 'white';
 
-        rooms[roomId] = { players, board: null, turn: 'red' };
-
-        io.to(data.fromId).to(socket.id).emit('start-game', {
-            roomId: roomId,
-            players: players
-        });
-    });
-
-    // Chat tizimi
-    socket.on('send-chat', (data) => {
-        socket.to(data.roomId).emit('receive-chat', {
-            sender: users[socket.id].name,
-            msg: data.msg
-        });
+        io.to(socket.id).emit('start-game', { roomId, players });
+        io.to(data.fromId).emit('start-game', { roomId, players });
     });
 
     socket.on('disconnect', () => {
@@ -74,6 +46,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Elite Server ${PORT}-portda tayyor!`);
-});
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
